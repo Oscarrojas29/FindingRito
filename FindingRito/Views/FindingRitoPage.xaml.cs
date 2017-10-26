4defe5f831d80d0a59ca95f219202866d3ed5fb2
oscarrojas29 <ing_oscarrojas@hotmail.com>
2017-10-25 20:44:58 -0700

    Initial check-in of module FindingRito

---
+++ FindingRito/Views/FindingRitoPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace FindingRito
+{
+    public partial class FindingRitoPage : ContentPage
+    {
+        public FindingRitoPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
